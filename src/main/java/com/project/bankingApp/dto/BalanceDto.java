f8b7f1ed77ed349b34d8b413126634b302ef7eb0
DELL <DELL@DESKTOP-QUTH88H>
2025-04-19 15:33:10 +0530

    DtoUpdate

---
+++ src/main/java/com/project/bankingApp/dto/BalanceDto.java
@@ -0,0 +1,12 @@
+package com.project.bankingApp.dto;
+
+import lombok.Data;
+
+@Data
+public class BalanceDto {
+	private long accNumber;
+	private String name;
+	private double amount;
+	
+	
+}
---
+++ src/main/java/com/project/bankingApp/dto/WithdrawDto.java
@@ -0,0 +1,10 @@
+package com.project.bankingApp.dto;
+
+import lombok.Data;
+
+@Data
+public class WithdrawDto {
+	private double amount;
+	private String name;
+	
+}
